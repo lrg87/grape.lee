15827987ff90fd0f430df582351dcd06df7d633f
lrg87 <ruigang.li@ele.me>
2017-04-06 16:03:20 +0800

    Update get_goproxy-1.py

--- get_goproxy-1.py
+++ get_goproxy-1.py
@@ -7,8 +7,8 @@
 import requests
 import json
 hlist=['xg-napos-order-1','xg-ecs-central-1']
-url = 'http://sash.elenet.me/api/sam/registries/'
-cookies= {"ubt_ssid":"t73hyebmh0acoszv4za4rhmitdpyu0p0_2016-12-15","COFFEE_TOKEN":"4c5174e4-e4f7-4513-bddd-3765877a275d","_ga":"GA1.2.772240705.1487681905"}
+url = ''
+cookies= {}
 '''requests的cookies格式是标准的字典格式，{"key1":"value","key2":"value"}'''
 for i in hlist:
     print(i)
