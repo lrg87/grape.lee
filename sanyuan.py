ee7960c902fe0c5793033d91d914027c82da347e
lrg87 <ruigang.li@ele.me>
2017-05-02 19:48:48 +0800

    Create sanyuan.py
    三元运算

---
+++ sanyuan.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+#@Time:2017/5/2 下午7:39
+#@Author:grape.lee
+#@File:py_06
+#@Software:PyCharm Community Edition
+'''
+三元运算
+'''
+# if 1==1:
+#     name = 'zhangsan'
+# else:
+#     name = 'lisi'
+
+
+
+name = "zhangsan" if 'a'=='a' else "lisi"
+print(name)
